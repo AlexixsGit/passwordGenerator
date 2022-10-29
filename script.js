e1b457bec700b466e910a3254991d59cd976f885
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2022-10-29 09:25:31 -0500

    Already generates password according to options checked

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-const lengthSlider = document.querySelector(".pass-length input");
+const lengthSlider = document.querySelector(".pass-length input"),
+    options = document.querySelectorAll(".option input"),
+    passwordInput = document.querySelector(".input-box input"),
+    generateBtn = document.querySelector(".generate-btn");
+
+const characters = { // object of letters, numbers & symbols
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "^!$%&|[](){}:;.,*+-#@<>~"
+}
+const generatePassword = () => {
+    let staticPassword = "",
+        randomPassword = "",
+        excludeDuplicate = false,
+        passLenth = lengthSlider.value;
+
+
+    options.forEach(option => {
+        if (option.checked) {
+            if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+                staticPassword += characters[option.id];
+            } else if (option.id === "spaces") {
+                staticPassword += `  ${staticPassword}  `;
+            } else {
+                excludeDuplicate = true;
+            }
+        }
+    })
+    for (let i = 0; i < passLenth; i++) {
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+
+
+        if (excludeDuplicate) {
+            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
+
+    }
+    passwordInput.value = randomPassword;
+}
 
 const updateSlider = () => {
     document.querySelector(".pass-length span").innerText = lengthSlider.value;
 }
 
+updateSlider();
+
 lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword)
